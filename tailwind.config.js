48f7b243a49887f9aa752a122d2d584ec129166d
DavlatbekNormatov <davlatbeknormatov17@gmail.com>
2023-02-06 21:59:42 +0500

    fodd

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        1353: "1353px",
+        1210: "1210px",
+        1142: "1142px",
+        1010: "1010px",
+        424: "424px",
+        364: "364px",
+        248: "248px",
+        386: "386px",
+      },
+
+      fontFamily: {
+        poppins: "Poppins"
+      },
+       
+      colors: {
+        brand: {
+          sariq: "#FF9401"
+        }
+      },
+
+      fontSize: {
+        52: "52px"
+      }
+    },
   },
   plugins: [],
 }
